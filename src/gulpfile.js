4757a4213ad55d0d716b2d92df13d6fa80963abf
Rob Matwiejczyk <rob@parall.ax>
2015-09-24 14:06:13 +0800

    bower & gulpfile added

---
+++ src/gulpfile.js
@@ -0,0 +1,74 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var elixir = require('laravel-elixir');
+var BrowserSync = require('laravel-elixir-browsersync2');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+ gulp.task("copyfiles", function(){
+
+    gulp.src("vendor/bower_dl/jquery/dist/jquery.js")
+        .pipe(gulp.dest("resources/assets/js/"));
+
+    // Copy datatables
+    var dtDir = 'vendor/bower_dl/datatables-plugins/integration/';
+
+    gulp.src("vendor/bower_dl/datatables/media/js/jquery.dataTables.js")
+        .pipe(gulp.dest('resources/assets/js/'));
+
+    gulp.src(dtDir + 'bootstrap/3/dataTables.bootstrap.css')
+        .pipe(rename('dataTables.bootstrap.scss'))
+        .pipe(gulp.dest('resources/assets/sass/others/'));
+
+    gulp.src(dtDir + 'bootstrap/3/dataTables.bootstrap.js')
+        .pipe(gulp.dest('resources/assets/js/'));
+
+    // Copy selectize
+    gulp.src("vendor/bower_dl/selectize/dist/css/**")
+        .pipe(gulp.dest("public/assets/selectize/css"));
+
+    gulp.src("vendor/bower_dl/selectize/dist/js/standalone/selectize.min.js")
+        .pipe(gulp.dest("public/assets/selectize/"));
+
+    // Copy pickadate
+    gulp.src("vendor/bower_dl/pickadate/lib/compressed/themes/**")
+        .pipe(gulp.dest("public/assets/pickadate/themes/"));
+
+    gulp.src("vendor/bower_dl/pickadate/lib/compressed/picker.js")
+        .pipe(gulp.dest("public/assets/pickadate/"));
+
+    gulp.src("vendor/bower_dl/pickadate/lib/compressed/picker.date.js")
+        .pipe(gulp.dest("public/assets/pickadate/"));
+
+    gulp.src("vendor/bower_dl/pickadate/lib/compressed/picker.time.js")
+        .pipe(gulp.dest("public/assets/pickadate/"));
+
+ });
+
+elixir(function(mix) {
+
+    BrowserSync.init();
+
+    mix.scripts([
+        'js/jquery.js',
+        'js/jquery.dataTables.js',
+        'js/dataTables.bootstrap.js'
+    ],
+    'public/assets/js/admin.js',
+    'resources/assets'
+    );
+
+    mix.sass('app.scss', 'public/assets/css/app.css')
+        .BrowserSync({
+            proxy: 'learntech.app'
+        });
+});
