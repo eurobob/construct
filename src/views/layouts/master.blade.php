c49a68117626ac9d2f4b56f1a57e315259a0af7e
Rob Matwiejczyk <rob@parall.ax>
2015-10-05 15:14:38 +0800

    shiv & respond cdn links

--- src/views/layouts/master.blade.php
+++ src/views/layouts/master.blade.php
@@ -15,8 +15,8 @@
 
   {{-- HTML5 Shim and Respond.js for IE8 support --}}
   <!--[if lt IE 9]>
-  <script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-  <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
   <![endif]-->
 </head>
 <body>
