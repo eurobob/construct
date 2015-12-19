203aea76262b7b568d188a70e6ebbe592510d506
Rob Matwiejczyk <rob@parall.ax>
2015-12-19 11:52:56 +0800

    html no-js class

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -4,12 +4,11 @@ namespace Livit\Build\Controllers;
 
 use Livit\Build\Controllers\AppController;
 
-
 class HomeController extends AppController
 {
     public function index()
     {
-        $page = $this->pageRepository->findByName('test');
+        $page = $this->pageRepository->findByName('home');
         return view('build::pages.home.index', ['page' => $page]);
     }
 }
