b7b46b3d928d8d78399852b6665e97ca824fa47d
Rob Matwiejczyk <rob@liv.it>
2016-01-26 20:29:25 +0800

    App Controller check for unauthorised users

--- src/Controllers/AppController.php
+++ src/Controllers/AppController.php
@@ -4,16 +4,20 @@ namespace Livit\Build\Controllers;
 
 use App\Http\Controllers\Controller;
 use Livit\Build\Repositories\PageRepository;
+use Livit\Build\User;
 
 class AppController extends Controller
 {
 
     protected $user;
+    protected $unauthorised;
 
     public function __construct(PageRepository $pageRepository)
     {
         $this->pageRepository = $pageRepository;
         $this->user = \Auth::user();
+        $this->unauthorised = count(User::whereAuthorised(0)->get());
         \View::share('user', $this->user);
+        \View::share('unauthorised', $this->unauthorised);
     }
 }
