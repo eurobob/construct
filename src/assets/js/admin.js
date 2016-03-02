026546cb3de812eddcce9a60db2790a6419e66cd
Rob Matwiejczyk <rob@liv.it>
2016-03-02 13:52:05 +0800

    Editable textarea typo

--- src/assets/js/admin.js
+++ src/assets/js/admin.js
@@ -6,7 +6,7 @@ Vue.use(require('vue-router'));
 Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#token').getAttribute('value');
 
 import editableText from './components/editableText.vue';
-import editableText from './components/editableTextarea.vue';
+import editableTextarea from './components/editableTextarea.vue';
 
 new Vue({
     el: '#app',
